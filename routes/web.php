e7ff9e74052b94a57daddf31aa1118edb13b9041
chitsusuhlaing <scm.chitsusuhlaing@gmail.com>
2021-02-08 10:58:51 +0630

    laravel6 comment system

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::view('/home', 'posts.create')->name('posts.create');
+Route::post('storePost', 'PostController@store')->name('posts.store');
+Route::get('posts', 'PostController@index')->name('posts.index');
+Route::get('show/{id}', 'PostController@show')->name('posts.show');
+Route::post('store', 'CommentController@store')->name('comments.store');
+
